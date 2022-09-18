8e569777056af974c4aabc2dd37ef0898a91c7f5
IAmRGroot <github@rgroot.nl>
2022-09-18 14:28:36 +0200

    Added initial software

---
+++ install.sh
@@ -0,0 +1,10 @@
+discord
+docker
+gnome-shell-extension-manager
+vscode
+linuxmint-keyring
+openfortivpn
+diodon
+
+hide top bar
+pop shell: https://github.com/pop-os/shell/issues/760#issuecomment-1120404930
